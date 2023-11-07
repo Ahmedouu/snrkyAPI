7204cd2393f5a0effc22c6bd429c3d2d60fdacbb
Ahmedouu <7medouu@gmail.com>
2023-11-07 19:59:31 +0800

    commit

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ so I was reading this https://scholar.dsu.edu/cgi/viewcontent.cgi?article=1426&c
 
 and thought it was too complicated for my small brain so I decided to do something with it in the hope of understanding what is going on.
 
-And got something out of it,  a nodejs API that uses zero knowledge verification in the server, the client is expected to send the verification key in a JSON body.
+And got something out of it,  a nodejs API that uses zero knowledge verification in the server, the client is expected to send the verification key in a JSON body... for now.
--- snrk.js
+++ snrk.js
@@ -1,26 +1,57 @@
 const snarkjs = require('snarkjs');
 const fs = require('fs');
-const { publicDecrypt } = require('crypto');
+const express = require('express');
+const app = express();
 
+//Generate the proof
 async function generate_proof() {
 
   const { proof, publicSignals } = await snarkjs.groth16.fullProve({in: 10}, "build/poseidon_hasher_js/poseidon_hasher.wasm", "circuit_0000.zkey");
-  //console.log(publicSignals);
-  //console.log(proof)
   return {proof, publicSignals};
 
 
 }
 
-async function verify(){ //the middleware
-  const vkey = JSON.parse(fs.readFileSync("verification_key.json"));
+//verify the proof with the key sent by the client.
+
+async function verify(vkey){ //verify the key against the proof
+  //const vkey = JSON.parse(fs.readFileSync("verification_key.json"));
+  if (!vkey){
+    return 
+  }
   const {proof, publicSignals} = await generate_proof();
   const res = await snarkjs.groth16.verify(vkey, publicSignals, proof);
   if ( res === true ){
     console.log("Verifcation okay!")
+    return true
   }
   else {
     console.log("Invalid proof")
+    return false;
   }
 }
-verify();
+
+app.use(express.json()); 
+
+// Middleware for authentication
+app.use(async (req, res, next) => {
+
+  const vkey = req.body; 
+  if (typeof vkey) {
+    res.status(400).send('Missing vkey in request body');
+  }
+  else if (await verify(vkey)) {
+    next(); // If the authentication is successful, proceed to the next middleware or route handler
+  } else {
+    res.status(401).send('Authentication failed'); // If authentication fails, send a 401 Unauthorized response
+  }
+});
+
+// Now, any route you define here will require your custom authentication to access
+app.get('/some_route', (req, res) => {
+  res.send('You accessed this route with successful authentication!');
+});
+
+app.listen(4000, ()=>{
+  console.log('I am listening')
+})
